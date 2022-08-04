fec19653c9504f37c0096af202f3fad1e0085d0f
jueun11 <jrangkim11@gmail.com>
2022-08-04 17:05:06 +0900

    경로수정

--- js/basket.js
+++ js/basket.js
@@ -1,9 +1,9 @@
 const basketPhoto = Array.from(document.getElementsByClassName('basketPhoto'));
 
 console.log(basketPhoto);
-basketPhoto[0].style.backgroundImage = "url(/css/image/basket/1.jpg)";
-basketPhoto[1].style.backgroundImage = "url(/css/image/basket/2.jpg)";
-basketPhoto[2].style.backgroundImage = "url(/css/image/basket/3.jpg)";
+basketPhoto[0].style.backgroundImage = "url(./css/image/basket/1.jpg)";
+basketPhoto[1].style.backgroundImage = "url(./css/image/basket/2.jpg)";
+basketPhoto[2].style.backgroundImage = "url(./css/image/basket/3.jpg)";
 
 const back = document.getElementsByClassName('back')[0];
 back.addEventListener('click',function(){
