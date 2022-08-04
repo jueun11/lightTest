ae6f906710a0004620cdd563cc5efaee18134c79
jueun11 <jrangkim11@gmail.com>
2022-08-04 16:51:02 +0900

    이미지파일 경로 변경

--- js/list.js
+++ js/list.js
@@ -1,12 +1,12 @@
 const listPhoto = Array.from(document.getElementsByClassName('listPhoto'));
 
 
-listPhoto[0].style.backgroundImage = "url(/css/image/list/1.jpg)";
-listPhoto[1].style.backgroundImage = "url(/css/image/list/2.jpg)";
-listPhoto[2].style.backgroundImage = "url(/css/image/list/3.jpg)";
-listPhoto[3].style.backgroundImage = "url(/css/image/list/4.jpg)";
-listPhoto[4].style.backgroundImage = "url(/css/image/list/5.jpg)";
-listPhoto[5].style.backgroundImage = "url(/css/image/list/6.jpg)";
+listPhoto[0].style.backgroundImage = "url(./css/image/list/1.jpg)";
+listPhoto[1].style.backgroundImage = "url(./css/image/list/2.jpg)";
+listPhoto[2].style.backgroundImage = "url(./css/image/list/3.jpg)";
+listPhoto[3].style.backgroundImage = "url(./css/image/list/4.jpg)";
+listPhoto[4].style.backgroundImage = "url(./css/image/list/5.jpg)";
+listPhoto[5].style.backgroundImage = "url(./css/image/list/6.jpg)";
 
 
 
--- js/page.js
+++ js/page.js
@@ -1,7 +1,7 @@
 const pageSlide = Array.from(document.getElementsByClassName('pageSlide'));
 
 console.log(pageSlide);
-pageSlide[0].style.backgroundImage = "url(/css/image/page/slide_1.jpg)";
+pageSlide[0].style.backgroundImage = "url(./css/image/page/slide_1.jpg)";
 
 
 
