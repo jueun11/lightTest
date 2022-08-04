caf7f76a0cb7c0972c114b44d2731b005ba4698f
jueun11 <jrangkim11@gmail.com>
2022-08-04 16:51:45 +0900

    경로변경:

--- js/order.js
+++ js/order.js
@@ -1,7 +1,7 @@
 const orderPhoto = Array.from(document.getElementsByClassName('orderPhoto'));
 
-orderPhoto[0].style.backgroundImage = "url(/css/image/order/1.jpg)";
-orderPhoto[1].style.backgroundImage = "url(/css/image/order/2.jpg)";
+orderPhoto[0].style.backgroundImage = "url(./css/image/order/1.jpg)";
+orderPhoto[1].style.backgroundImage = "url(./css/image/order/2.jpg)";
 
 const back = document.getElementsByClassName('back')[0];
 
